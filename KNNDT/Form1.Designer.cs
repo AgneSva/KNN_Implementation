9f4d464202d680815c29681a1c42328451bf19b1
Agnė Švambarytė <s037681@ad.viko.lt>
2021-09-16 13:57:35 +0300

    Add project files.

---
+++ KNNDT/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace KNNDT
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.dgv2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgv3 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rtbDistance = new System.Windows.Forms.RichTextBox();
+            this.rbtIndex = new System.Windows.Forms.RichTextBox();
+            this.rbtClass = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.x = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(40, 389);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 0;
+            this.start.Text = "klasifikuoti";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(40, 33);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.RowTemplate.Height = 25;
+            this.dgv1.Size = new System.Drawing.Size(394, 199);
+            this.dgv1.TabIndex = 1;
+            // 
+            // dgv2
+            // 
+            this.dgv2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv2.Location = new System.Drawing.Point(40, 263);
+            this.dgv2.Name = "dgv2";
+            this.dgv2.RowTemplate.Height = 25;
+            this.dgv2.Size = new System.Drawing.Size(394, 111);
+            this.dgv2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 419);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "rezultatas:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "klasifikuoti objektai:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "objektai klasifikavimui:";
+            // 
+            // dgv3
+            // 
+            this.dgv3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv3.Location = new System.Drawing.Point(40, 446);
+            this.dgv3.Name = "dgv3";
+            this.dgv3.RowTemplate.Height = 25;
+            this.dgv3.Size = new System.Drawing.Size(394, 118);
+            this.dgv3.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"});
+            this.comboBox1.Location = new System.Drawing.Point(244, 410);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(218, 414);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "k=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(565, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "distancija tarp tasku:";
+            // 
+            // rtbDistance
+            // 
+            this.rtbDistance.Location = new System.Drawing.Point(565, 51);
+            this.rtbDistance.Name = "rtbDistance";
+            this.rtbDistance.Size = new System.Drawing.Size(330, 96);
+            this.rtbDistance.TabIndex = 11;
+            this.rtbDistance.Text = "";
+            // 
+            // rbtIndex
+            // 
+            this.rbtIndex.Location = new System.Drawing.Point(565, 190);
+            this.rbtIndex.Name = "rbtIndex";
+            this.rbtIndex.Size = new System.Drawing.Size(330, 96);
+            this.rbtIndex.TabIndex = 12;
+            this.rbtIndex.Text = "";
+            // 
+            // rbtClass
+            // 
+            this.rbtClass.Location = new System.Drawing.Point(565, 330);
+            this.rbtClass.Name = "rbtClass";
+            this.rbtClass.Size = new System.Drawing.Size(330, 96);
+            this.rbtClass.TabIndex = 13;
+            this.rbtClass.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(587, 457);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 15);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "label6";
+            // 
+            // x
+            // 
+            this.x.HeaderText = "Column1";
+            this.x.Name = "x";
+            // 
+            // y
+            // 
+            this.y.HeaderText = "Column1";
+            this.y.Name = "y";
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "x";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "y";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "x";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "y";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1305, 600);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.rbtClass);
+            this.Controls.Add(this.rbtIndex);
+            this.Controls.Add(this.rtbDistance);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dgv3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv2);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.DataGridView dgv2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgv3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox rtbDistance;
+        private System.Windows.Forms.RichTextBox rbtIndex;
+        private System.Windows.Forms.RichTextBox rbtClass;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn x;
+        private System.Windows.Forms.DataGridViewTextBoxColumn y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+    }
+}
+
---
+++ KNNDT/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace KNNDT
+{
+    public partial class Form1 : Form
+    {
+        string conString = @"Data Source=LAPTOP-2DBCAKJA\SQLEXPRESS;Initial Catalog=knn;Integrated Security=True";
+        //  List<double> x = new List<double>();
+        int k;
+        int lastNumber;
+        string result;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            //  using (SqlConnection sqlCon = new SqlConnection(conString))
+            //{
+            //filling tables
+            dgv1.ReadOnly = true;
+           // dgv2.ReadOnly = true;
+
+
+            SqlDataAdapter adapter = new SqlDataAdapter("Select * from KLASIFIKUOTI", conString);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dgv1.DataSource = dt;
+
+            SqlDataAdapter adapter2 = new SqlDataAdapter("Select * from NEKLASIFIKUOTI", conString);
+            DataTable dt2 = new DataTable();
+            adapter2.Fill(dt2);
+            dgv2.DataSource = dt2;
+            dgv3.DataSource = dt2;
+
+        }
+        //funkcija skaiciuoti distan.
+        private double distanceToCenterFunction(double x1Test, double x2Test, double x1Data, double x2Data)
+        {
+            var result = Math.Sqrt(Math.Pow(x1Test - x1Data, 2) + Math.Pow(x2Test - x2Data, 2));
+            return result;
+        }
+
+        public void start_Click(object sender, EventArgs e)
+        {
+            int rc = 0;
+           
+                double[] x1Data = new double[dgv1.RowCount - 1];
+                double[] x2Data = new double[dgv1.RowCount - 1];
+                string[] classData = new string[dgv1.RowCount - 1];
+                double[] distance = new double[dgv1.RowCount - 1];
+                double[] xTest1 = new double[dgv2.RowCount - 1];
+                double[] xTest2 = new double[dgv2.RowCount - 1];
+
+
+                for (int i = 0; i < dgv2.RowCount - 1; i++)
+                {
+                    xTest1[i] = Convert.ToDouble(dgv2.Rows[i].Cells[0].Value);
+                    xTest2[i] = Convert.ToDouble(dgv2.Rows[i].Cells[1].Value);
+
+                }
+
+                for (int i = 0; i < dgv1.RowCount - 1; i++)
+                {
+                    x1Data[i] = Convert.ToDouble(dgv1.Rows[i].Cells[0].Value);
+                    x2Data[i] = Convert.ToDouble(dgv1.Rows[i].Cells[1].Value);
+
+                    classData[i] = Convert.ToString(dgv1.Rows[i].Cells[2].Value);
+
+
+                distance[i] = distanceToCenterFunction(xTest1[0], xTest2[0], x1Data[i], x2Data[i]);
+
+                }
+
+
+                //find k
+                k = Convert.ToInt32(comboBox1.SelectedItem);
+
+                double[] distanceCopy = new double[dgv1.RowCount - 1];
+                double[] distanceAscending = new double[dgv1.RowCount - 1];
+                double[] distanceAscendingIndexNumber = new double[dgv1.RowCount - 1];
+                double smallest = 9999;
+
+                foreach (var item in distance)
+                {
+                    distanceCopy = distance;
+                }
+
+                for (int j = 0; j < dgv1.RowCount - 1; j++)
+                {
+                    for (int i = 0; i < dgv1.RowCount - 1; i++)
+                    {
+                        if (smallest > distanceCopy[i])
+                        {
+                            smallest = distanceCopy[i];
+                            distanceAscending[j] = smallest;
+                            distanceAscendingIndexNumber[j] = i;
+                            lastNumber = i;
+                        }
+                    }
+                    distanceCopy[lastNumber] = 9999;
+                    smallest = 9999;
+                }
+
+
+                rtbDistance.Text = null;
+                rbtIndex.Text = null;
+                rbtClass.Text = null;
+                List<string> classList = new List<string>();
+                for (int i = 0; i < k; i++)
+                {
+                    rtbDistance.AppendText(distanceAscending[i].ToString() + Environment.NewLine);
+                    rbtIndex.AppendText(distanceAscendingIndexNumber[i].ToString() + Environment.NewLine);
+                    rbtClass.AppendText(classData[Convert.ToInt32(distanceAscendingIndexNumber[i])] + Environment.NewLine);
+                    classList.Add(classData[Convert.ToInt32(distanceAscendingIndexNumber[i])]);
+
+                }
+
+                var plusc = classList.Where(s => s.Contains("PLUS")).Count();
+                var minusc = classList.Where(s => s.Contains("MINUS")).Count();
+
+                if (plusc > minusc)
+                {
+                    result = "PLUS";
+                }
+                else if (minusc > plusc)
+                {
+                    result = "MINUS";
+                }
+                else
+                {
+                    result = "NEAISKU";
+                }
+
+                //filling table with results
+                dgv3.Rows[rc].Cells[2].Value = result;
+                rc ++;
+            
+
+        }
+
+
+    }
+}
